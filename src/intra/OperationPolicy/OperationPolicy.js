e1c85529c9d59d81262894311539c52880188536
wodnd0131 <wodnd0131@naver.com>
2023-11-24 01:24:31 +0900

    fix:OperationPolicy

--- src/intra/OperationPolicy/OperationPolicy.js
+++ src/intra/OperationPolicy/OperationPolicy.js
@@ -4,10 +4,18 @@ export const save = async (data) => {
   return await request.post(`OperationPolicy`, data);
 };
 export const recommand = async (data) => {
-  return await request.post(`OperationPolicy/recommand`, data);
+  return await request.post(`OperationPolicy/recommand`, JSON.stringify(data), {
+      headers: {
+          'Content-Type': 'application/json'
+      }
+  });
 };
 export const pass = async (data) => {
-  return await request.post(`OperationPolicy/pass`, data);
+  return await request.post(`OperationPolicy/pass`, JSON.stringify(data), {
+    headers: {
+        'Content-Type': 'application/json'
+    }
+});
 };
 export const getAll = async () => {
   return await request.get(`OperationPolicy/getAll`);
