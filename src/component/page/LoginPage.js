ab333f1d4bcad27b4ce39c6ff5462eba928e74fa
wodnd0131 <wodnd0131@naver.com>
2023-12-03 20:06:47 +0900

    feat:fee,recommand in sellgroup

--- src/component/page/LoginPage.js
+++ src/component/page/LoginPage.js
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom";
 import Login from "../../user/customer/Login";
 import AuthPage from "../../component/page/AuthPage";
-import RegisterGoogle from "../../user/customer/RegisterGoogle";
 
 const LoginPage = () => {
   return (
@@ -11,7 +10,6 @@ const LoginPage = () => {
       <Link to="/register">
           <button>회원가입</button>
       </Link>
-      <RegisterGoogle/>
     </div>
   );
 };
