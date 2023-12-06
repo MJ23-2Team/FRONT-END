b148bcf7c58a053e3e8dcc740d07668d2d836958
wodnd0131 <wodnd0131@naver.com>
2023-12-06 19:11:00 +0900

    fix:apiResponse

--- src/user/customer/Login.js
+++ src/user/customer/Login.js
@@ -1,7 +1,6 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { loginUser } from "./Customer";
-import { getById } from "../employee/adviceNote/AdviceNote";
 
 const Login = () => {
   const navigate = useNavigate();
