9735419e104ff842023fc3861db2ed93a43e461e
wodnd0131 <wodnd0131@naver.com>
2023-12-14 14:12:36 +0900

    feat:AWS

--- src/user/customer/RegisterCustomer.js
+++ src/user/customer/RegisterCustomer.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { setInfo } from "./Customer";
+import "../../component/common/Register.css";
   const RegisterCustomer = () => {
 
   const navigate = useNavigate();
@@ -25,7 +26,7 @@ import { setInfo } from "./Customer";
   };
 
   return (
-    <>
+    <div className="page">
       <div>고객 정보 등록</div>
       <input type="text" name="address" placeholder="address" onChange={(e) => onHandleChangedata(e)} />
       <input type="text" name="age" placeholder="age" onChange={(e) => onHandleChangedata(e)} />
@@ -41,7 +42,10 @@ import { setInfo } from "./Customer";
       <button onClick={() => {onSubmitHandle();}}>
         등록하기
       </button>
-    </>
+    </div>
   );
+
 };
+
+
 export default RegisterCustomer;
